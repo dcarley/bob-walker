02ccebcdccd50862b534dd7529f868601d2f4184
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-09-05 18:08:48 +0100

    Sport and Christmas modes!

--- bob-walker.ps1
+++ bob-walker.ps1
@@ -1,5 +1,16 @@
-while($true){
-   Write-Host "beard"
-   Write-Host "beer"
-   Write-Host "pie"
-}
+param(
+  [string]$s = "cricket",
+  [string]$x = "mince pie"
+)
+
+while ($true){
+  Write-Host "beard"
+  Write-Host "beer"
+  Write-Host "pie"
+    If ($s -eq $true) {
+      Write-Host $s
+    }
+    If ($x -eq $true) {
+      Write-Host $x
+    }
+ }

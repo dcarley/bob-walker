fbe862b38b788755b79ad86a6fa62f2c8d9e654b
Anna Shipman <anna@annashipman.co.uk>
2014-10-03 21:41:12 +0100

    Use camel case not snake case
    As is idiomatic for Scala.
    
    Also, no need for this to be a case class as it doesn't store data, and no need for it to be final, really.

--- bob-walker.scala
+++ bob-walker.scala
@@ -1,47 +1,47 @@
-final case object bobwalker {
+object bobWalker {
 
   def main(args: Array[String]) {
-    val bob_modes = process(args)
-    start_bob(bob_modes)
+    val bobModes = process(args)
+    startBob(bobModes)
   }
 
   def process(args: Array[String]): Array[String] = {
-    val all_args: Array[Char] = args.flatMap(x => x)
-    val bob_modes = for {
-      x <- all_args
-    } yield add_mode(x)
-    bob_modes.flatMap(x => x)
+    val allArgs: Array[Char] = args.flatMap(x => x)
+    val bobModes = for {
+      x <- allArgs
+    } yield addMode(x)
+    bobModes.flatMap(x => x)
   }
 
-  def add_mode(letter: Char): Array[String] = {
-    var new_modes: Array[String] = Array[String]("beard", "beer", "pie")
+  def addMode(letter: Char): Array[String] = {
+    var newModes: Array[String] = Array[String]("beard", "beer", "pie")
     letter match {
       case 'd' =>
         println("Drunk mode not yet enabled")
       case 'p' =>
-        new_modes = new_modes :+ "chef" :+ "perl" :+ "CPAN"
+        newModes = newModes :+ "chef" :+ "perl" :+ "CPAN"
       case 's' => 
-        new_modes = new_modes :+ "rugby" :+ "cricket"
+        newModes = newModes :+ "rugby" :+ "cricket"
       case 'x' =>
-        new_modes = new_modes :+ "mince pies" :+ "reindeer"
+        newModes = newModes :+ "mince pies" :+ "reindeer"
       case _   =>
         // if we do not have a case for the character
         // then do nothing
     }
-    new_modes
+    newModes
   }
 
-  def start_bob(bob_modes: Array[String]) = {
+  def startBob(bobModes: Array[String]) = {
     var i = 0
     while (i < 1000000000) {
-      print_bob(bob_modes)
+      printBob(bobModes)
       i = i + 1
     }
   }
 
-  def print_bob(bob_modes: Array[String]) = {
+  def printBob(bobModes: Array[String]) = {
     for {
-      x <- bob_modes
+      x <- bobModes
     } println(x)
   }
 
